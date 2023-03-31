895406f6d44e369104892102fa73ecf434557ed6
zarko5 <zarkohajder1@gmail.com>
2023-03-31 22:59:59 +0200

    pong_tastatura -- Formatiranje i komentari

--- projekti/pong/pong_tastatura/Verilog2.v
+++ projekti/pong/pong_tastatura/Verilog2.v
@@ -1,6 +1,6 @@
 module one_sec(input cl, output reg [32:0] s);
 	reg [32:0] counter;
-
+// od ovog modula zavisi brzina logike igre u sustini i detektovanje keypressova
 	always @(posedge cl) begin
 		if (counter > 33'd3_000_000) begin
 			counter = 33'd0;
